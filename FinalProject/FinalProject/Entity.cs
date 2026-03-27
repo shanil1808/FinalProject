0730402cbea60430f144c4342948c1d94b3c1975
Shanil <shanil180899@gmail.com>
2026-03-27 12:55:36 -0400

    Entity not added

--- FinalProject/FinalProject/Entity.cs
+++ FinalProject/FinalProject/Entity.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
-    internal class Entity
+    public abstract class Entity
     {
+        private int id;
+        private string name;
+        DateTime createdDate;
+
+        public Entity(int id, string name, DateTime createdDate)
+        {
+            this.id = id;
+            this.name = name;
+            this.createdDate = createdDate;
+        }
+        public int GetId() { return id; }
+
+        public string GetName() { return name; }
+
+        public DateTime GetCreatedDate() { return createdDate; }
+
+        public void SetName(string name)
+        {
+            if (name == null) throw new ArgumentNullException("name"); 
+            this.name = name;
+        }
+
+        public void SetCreatedDate(DateTime dateTime) 
+        { 
+            this.createdDate = dateTime; 
+        }
+
+        public virtual bool Validate()
+        {
+            if (name == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public abstract void Display();
     }
 }
